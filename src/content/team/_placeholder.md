8069f8c1c3615eccb1fc4f0044cce0424664b45f
Jean Luca Bez <jeanlucabez@gmail.com>
2026-06-27 19:42:06 -0700

    remove deprecated files

--- src/content/team/_placeholder.md
+++
@@ -1,5 +0,0 @@
----
-name: "TODO: confirm team member"
-role: "TODO"
-draft: true
----
--- tests/e2e/features.spec.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from '@playwright/test';
-test('features lists 6 dimensions', async ({ page }) => {
-  await page.goto('/features');
-  await expect(page.getByTestId('dimensions').locator('> div')).toHaveCount(6);
-});
